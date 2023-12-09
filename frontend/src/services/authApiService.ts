2231bb14bebf95c9455a79e481a2e9beb9d3ba1d
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-12-09 22:48:00 +0530

    add : files

--- frontend/src/services/authApiService.ts
+++ frontend/src/services/authApiService.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApi = createApi({
   reducerPath: "userAuthAPI",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:8000/auth/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://hospital-authentication.vercel.app/auth/" }),
   endpoints: (builder) => ({
     registerUser: builder.mutation({
       query: (user) => {
