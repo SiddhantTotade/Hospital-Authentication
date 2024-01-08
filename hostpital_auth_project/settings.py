c6f02dadd7089a19ef860dbd8c95da3f8f727e15
SiddhantTotade <siddhanttotade.1994@gmail.com>
2024-01-08 17:11:36 +0530

    modify : settings.py

--- hostpital_auth_project/settings.py
+++ hostpital_auth_project/settings.py
@@ -107,21 +107,21 @@ WSGI_APPLICATION = "hostpital_auth_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "hospital_db",
-        "USER": "Siddhant",
-        "PASSWORD": env("DATABASE_PASSWORD"),
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
-        "OPTIONS": {
-            "sql_mode": "traditional",
-        }
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "NAME": "hospital_db",
+    #     "USER": "Siddhant",
+    #     "PASSWORD": env("DATABASE_PASSWORD"),
+    #     "HOST": "127.0.0.1",
+    #     "PORT": "3306",
+    #     "OPTIONS": {
+    #         "sql_mode": "traditional",
+    #     }
+    # }
 }
 
 
